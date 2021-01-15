46277b43e6f5fc36ceb5f64977f38791fe0d459a
Bamidele Michael <mbamidele29@gmail.com>
2021-01-15 22:02:28 +0100

    added responsiveness

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
-    const main=$('.about');
+    const main=$('.main');
+    const prev=$('.prev');
     main.toggleClass('back');
     const link=$('.link');
     link.on('click', function(){
@@ -9,5 +10,12 @@ $(document).ready(function(){
 
         $('.biz-card .'+target).toggleClass('hidden', false);
         $('.biz-card').toggleClass('back', true);
+
+        prev.toggleClass('hidden', false);
+    });
+
+    prev.on('click', function(){
+        prev.toggleClass('hidden', true);
+        $('.biz-card').toggleClass('back', false);
     });
 });
